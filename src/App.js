39cdb9507630163f02046c5ad8e75eadaa6667c5
weshsnr <ndungugeoffrey11@gmail.com>
2020-11-22 17:39:35 +0300

    initial commit - hooks workflow

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import './App.css';
+import Card from './card.component';
 
-function App() {
+const Button = styled.button`
+  background-color: greenyellow;
+  color: black;
+  padding: 10px;
+  font-size: 15px;
+  border-radius: 8px;
+  border: none;
+  cursor: pointer;
+  margin: 20px 0px 0px 0px;
+`;
+
+const App =() => {
+
+  const [user, setUser] = useState(null)
+  const [searchQuery, setsearchQuery] = useState('Kamren');
+
+  useEffect(() => {
+    const fetchFunc = async () => {
+      const searchTerm = searchQuery.charAt(0).toLocaleUpperCase() + searchQuery.slice(1)
+      console.log(searchTerm)
+      const response = await fetch(`https://jsonplaceholder.typicode.com/users?username=${searchTerm}`);
+      const resJson =  await response.json();
+
+      setUser(resJson[0])
+    }
+    fetchFunc();
+    return () => {} // cleanup purposes
+  }, [searchQuery])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Card>
+        <div>
+          <input type='text' value={searchQuery} onChange={event => setsearchQuery(event.target.value)}/>         
+        </div>
+
+        <Button>Search</Button>
+        {user ?
+        <div>
+          <h2>{user.name}</h2>
+          <h3>{user.username}</h3>
+          <h3>{user.email}</h3>
+          <h4>{user.website}</h4>
+          <h5>{user.address.street}</h5>
+          <h6>{user.company.name}</h6>
+        </div> : <p>User not found</p>}      
+      </Card>
   );
 }
 
---
+++ src/card.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const CardDiv = styled.div`
+background-color: aliceblue;
+border: 1px solid blueviolet;
+border-radius: 10px;
+text-align: center;
+height: auto;
+margin:  50px auto 0px auto;
+width: 60%;
+color: #3e9959;
+padding: 30px;
+`;
+
+
+const Card = (props) => (
+    <CardDiv>
+        {props.children}
+    </CardDiv>
+)
+
+
+export default Card
